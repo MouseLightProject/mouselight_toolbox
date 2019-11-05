f3e21cb8cd085f78f89e3e2aa47ff81489b46c49
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-11-05 13:38:59 -0500

    Fixed bug, added another pred function

--- does_file_name_not_end_in_dot_tif.m
+++ does_file_name_not_end_in_dot_tif.m
@@ -1,3 +1,3 @@
 function result = does_file_name_not_end_in_dot_tif(file_name)
-    result = ~(does_match_regexp(file_name, '.tif$')) ;
+    result = ~(does_match_regexp(file_name, '\.tif$')) ;
 end
