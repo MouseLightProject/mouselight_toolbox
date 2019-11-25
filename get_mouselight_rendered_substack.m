a566283ecec90f6b16b45d71a77810431c35ca4e
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-11-25 16:23:18 -0500

    Handle nonexistent rendered tiles properly in get_mouselight_rendered_substack.m

--- get_mouselight_rendered_substack.m
+++ get_mouselight_rendered_substack.m
@@ -8,15 +8,17 @@ function substack = get_mouselight_rendered_substack(rendered_folder_name, chann
     % of full stack *at the desired zoom level*.
     parameter_file_name = fullfile(rendered_folder_name, 'calculated_parameters.jl') ;
     parameters = read_renderer_calculated_parameters_file(parameter_file_name) ;
-    level_step_count = parameters.level_step_count ;
+    maximum_zoom_level = parameters.level_step_count ;
     chunk_shape_ijk = parameters.leaf_shape ;  % xyz order
     if ~exist('zoom_level', 'var') || isempty(zoom_level) ,
-        zoom_level = level_step_count ;
+        zoom_level = maximum_zoom_level ;
     end
     % note that chunk shape (in voxels) does not change depending on zoom
     % level.  It's just that at zoom level 0 there is 1==8^0 chunk, at zoom level 1
     % there are 8==8^1 chunks, at zoom level 2 there are 64==8^2 chunks,
     % etc
+    
+    chunks_per_dimension_ijk = 2^zoom_level ;  % same for x, y, and z
     
     %substack_far_corner_ijk1 = substack_origin_ijk1 + substack_shape_ijk - 1 ;  % coords of the voxel in the substack with the largest coords    
     [substack_chunk_origin_ijk1, substack_shape_in_chunks_ijk] = ...
@@ -29,17 +31,30 @@ function substack = get_mouselight_rendered_substack(rendered_folder_name, chann
             for k1_chunk_within_substack = 1 : substack_shape_in_chunks_ijk(3) ,
                 ijk1_chunk_within_substack = [i1_chunk_within_substack j1_chunk_within_substack k1_chunk_within_substack] ;
                 ijk1_chunk_within_stack = substack_chunk_origin_ijk1 + ijk1_chunk_within_substack - 1 ;
-                octree_path = octree_paths_from_chunk_ijk1s(ijk1_chunk_within_stack, zoom_level) ;
-                chunk = load_mouselight_rendered_chunk(rendered_folder_name, octree_path, channel_index) ;
-                ijk1_chunk_origin_within_substack = chunk_shape_ijk .* (ijk1_chunk_within_substack-1) + 1 ;
-                ijk1_chunk_far_corner_within_substack = ijk1_chunk_origin_within_substack + chunk_shape_ijk - 1 ;
-                chunk_aligned_substack(ijk1_chunk_origin_within_substack(2):ijk1_chunk_far_corner_within_substack(2) , ...
-                                       ijk1_chunk_origin_within_substack(1):ijk1_chunk_far_corner_within_substack(1) , ...
-                                       ijk1_chunk_origin_within_substack(3):ijk1_chunk_far_corner_within_substack(3)) = chunk ;
+                if all(1<=ijk1_chunk_within_stack & ijk1_chunk_within_stack<=chunks_per_dimension_ijk) ,
+                    octree_path = octree_paths_from_chunk_ijk1s(ijk1_chunk_within_stack, zoom_level) ;
+                    try
+                        chunk = load_mouselight_rendered_chunk(rendered_folder_name, octree_path, channel_index) ;
+                        ijk1_chunk_origin_within_substack = chunk_shape_ijk .* (ijk1_chunk_within_substack-1) + 1 ;
+                        ijk1_chunk_far_corner_within_substack = ijk1_chunk_origin_within_substack + chunk_shape_ijk - 1 ;
+                        chunk_aligned_substack(ijk1_chunk_origin_within_substack(2):ijk1_chunk_far_corner_within_substack(2) , ...
+                                               ijk1_chunk_origin_within_substack(1):ijk1_chunk_far_corner_within_substack(1) , ...
+                                               ijk1_chunk_origin_within_substack(3):ijk1_chunk_far_corner_within_substack(3)) = chunk ;
+                    catch err
+                        if isequal(err.identifier, 'mouselight:no_such_rendered_chunk') ,
+                            % do nothing---load_mouselight_rendered_chunk()
+                            % throws this if a chunk does not exist
+                        else
+                            rethrow(err) ;
+                        end
+                    end
+                            
+                end
             end
         end
     end
-    
+
+    % Cut out just the desired part
     chunk_aligned_substack_origin_ijk1 = (substack_chunk_origin_ijk1-1) .* chunk_shape_ijk + 1 ;
     substack_origin_within_chunk_aligned_substack_ijk1 = substack_origin_ijk1 - chunk_aligned_substack_origin_ijk1 + 1 ;
     substack_far_corner_within_chunk_aligned_substack_ijk1 = substack_origin_within_chunk_aligned_substack_ijk1 + substack_shape_ijk - 1 ;
--- load_mouselight_rendered_chunk.m
+++ load_mouselight_rendered_chunk.m
@@ -3,5 +3,10 @@ function stack = load_mouselight_rendered_chunk(rendered_folder_name, chunk_path
     chunk_folder_name = fullfile(rendered_folder_name, chunk_path_as_string) ;
     chunk_file_leaf_name = sprintf('default.%d.tif', channel_index) ;
     chunk_file_name = fullfile(chunk_folder_name, chunk_file_leaf_name) ;
-    stack = read_16bit_grayscale_tif(chunk_file_name) ;
+    if exist(chunk_file_name, 'file') ,
+        stack = read_16bit_grayscale_tif(chunk_file_name) ;
+    else
+        error('mouselight:no_such_rendered_chunk', ...
+              'No rendered chunk at %s', chunk_file_name) ;
+    end
 end
