2a554bf093d096b40129121d68f286afe3ed6c9e
Adam L. Taylor <adamlyletaylor@gmail.com>
2020-08-06 21:03:14 -0400

    Added functions for running whole fragment pipeline as one script

---
+++ generate_fragments_higher_level.m
@@ -0,0 +1,53 @@
+function generate_fragments_higher_level(file_format, ...
+                                         minimum_centerpoint_count_per_fragment, ...
+                                         bounding_box_low_corner_xyz, ...
+                                         bounding_box_high_corner_xyz)
+    % Set paths, etc                                             
+    input_folder_path = ...
+        sprintf('/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/%s/build-brain-output/full-as-named-tree-mats', ...
+                sample_date) ;
+    output_folder_path = ...
+        sprintf('/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/%s/build-brain-output/frags-with-5-or-more-nodes', ...
+                sample_date) ;       
+    maximum_core_count_desired = inf ;
+                                             
+    % Get the pool ready
+    use_this_many_cores(maximum_core_count_desired) ;
+    
+    if ~exist(output_folder_path, 'dir') ,
+        mkdir(output_folder_path) ;
+    end
+    full_tree_file_names = simple_dir(fullfile(input_folder_path, 'auto-cc-*.mat')) ;
+    full_trees_to_process_count = length(full_tree_file_names) ;
+    tic_id = tic() ;
+    fprintf('Starting the big parfor loop, going to process %d full trees...\n', full_trees_to_process_count) ;
+    parfor_progress(full_trees_to_process_count) ;
+    for full_tree_index = 1 : full_trees_to_process_count ,
+        full_tree_file_name = full_tree_file_names{full_tree_index} ;
+        full_tree_mat_file_path = fullfile(input_folder_path, full_tree_file_name) ;
+        named_tree = load_named_tree_from_mat(full_tree_mat_file_path) ;
+        generate_fragments_from_named_tree(output_folder_path, ...
+                                           named_tree, ...
+                                           file_format, ...
+                                           minimum_centerpoint_count_per_fragment, ...
+                                           bounding_box_low_corner_xyz, ...
+                                           bounding_box_high_corner_xyz) ;
+        
+        % Update the progress bar
+        parfor_progress() ;
+    end
+    parfor_progress(0) ;
+    toc(tic_id) ;
+
+    % Count the number of fragments
+    command_line = sprintf('ls -U "%s" | wc -l', output_folder_path) ;
+    [status, stdout] = system(command_line) ;
+    if status ~= 0 ,
+        error('There was a problem running the command %s.  The return code was %d', command_line, status) ;
+    end
+    fragment_count = str2double(stdout) ;
+    if ~isfinite(fragment_count) ,
+        error('There was a problem running the command %s.  Unable to parse output.  Output was: %s', command_line, stdout) ;
+    end    
+    fprintf('Fragment count: %d\n', fragment_count) ;
+end
