c35d1fbf671e77e620094ae7bd4892d4cd1f327f
Adam L. Taylor <adamlyletaylor@gmail.com>
2020-08-13 16:26:18 -0400

    Fixed generate_fragments_higher_level.m

--- generate_fragments_higher_level.m
+++ generate_fragments_higher_level.m
@@ -1,7 +1,12 @@
-function generate_fragments_higher_level(file_format, ...
+function generate_fragments_higher_level(sample_date, ...
+                                         file_format, ...
                                          minimum_centerpoint_count_per_fragment, ...
                                          bounding_box_low_corner_xyz, ...
                                          bounding_box_high_corner_xyz)
+                                     
+    fprintf('\n\nGenerating fragments...\n') ;                                     
+    outer_tic_id = tic() ;
+                                     
     % Set paths, etc                                             
     input_folder_path = ...
         sprintf('/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/%s/build-brain-output/full-as-named-tree-mats', ...
@@ -19,9 +24,10 @@ function generate_fragments_higher_level(file_format, ...
     end
     full_tree_file_names = simple_dir(fullfile(input_folder_path, 'auto-cc-*.mat')) ;
     full_trees_to_process_count = length(full_tree_file_names) ;
-    tic_id = tic() ;
-    fprintf('Starting the big parfor loop, going to process %d full trees...\n', full_trees_to_process_count) ;
-    parfor_progress(full_trees_to_process_count) ;
+    %tic_id = tic() ;
+    fprintf('Starting the for loop to process trees, going to process %d full trees...\n', full_trees_to_process_count) ;
+    pbo = progress_bar_object(full_trees_to_process_count) ;
+    %parfor_progress(full_trees_to_process_count) ;
     for full_tree_index = 1 : full_trees_to_process_count ,
         full_tree_file_name = full_tree_file_names{full_tree_index} ;
         full_tree_mat_file_path = fullfile(input_folder_path, full_tree_file_name) ;
@@ -34,10 +40,12 @@ function generate_fragments_higher_level(file_format, ...
                                            bounding_box_high_corner_xyz) ;
         
         % Update the progress bar
-        parfor_progress() ;
+        %parfor_progress() ;
+        pbo.update(full_tree_index) ;
     end
-    parfor_progress(0) ;
-    toc(tic_id) ;
+    %parfor_progress(0) ;
+    pbo.finish_up() ;
+    %toc(tic_id) ;
 
     % Count the number of fragments
     command_line = sprintf('ls -U "%s" | wc -l', output_folder_path) ;
@@ -50,4 +58,7 @@ function generate_fragments_higher_level(file_format, ...
         error('There was a problem running the command %s.  Unable to parse output.  Output was: %s', command_line, stdout) ;
     end    
     fprintf('Fragment count: %d\n', fragment_count) ;
+    
+    elapsed_time = toc(outer_tic_id) ;
+    fprintf('Done generating fragments.  Elapsed time to generate fragments: %g sec.\n', elapsed_time) ;
 end
