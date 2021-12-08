c0661a225faa5a2072c3dcd4225e494f407c1e01
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-12-08 13:58:41 -0500

    Deleted everything

--- absolute_filename.m
+++
@@ -1,9 +0,0 @@
-function filename_abs = absolute_filename(filename)
-
-    if is_filename_absolute(filename)
-        filename_abs = filename ;
-    else
-        filename_abs = fullfile(pwd(), filename) ;
-    end
-
-end
--- test_progress_bar_object.m
+++
@@ -1,7 +0,0 @@
-function test_progress_bar_object()
-    n = 10000 ;
-    pbo = progress_bar_object(n) ;
-    for i = 1 : n ,
-        pbo.update(i) ;
-    end
-end
