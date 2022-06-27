a666ffb760aa4ff3db8f7a0f91e4dfea1ecf896f
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-06-27 15:05:34 -0400

    Fixed bug with empty input to load_landmark_file()

--- load_landmark_file.m
+++ load_landmark_file.m
@@ -3,6 +3,8 @@ function [ijk0_from_landmark_index, foreground_p_value_from_landmark_index, imag
     landmark_data = load_tabular_data(landmark_file_name) ;
     if isempty(landmark_data) ,
         ijk0_from_landmark_index = zeros(0,3) ;
+        foreground_p_value_from_landmark_index = zeros(0,1) ;
+        imagery_value_from_landmark_index = zeros(0,1) ;
     else
         flipped_ijk0_from_landmark_index = landmark_data(:,1:3) ;
         % the landmark-detection code is run on the the raw (flipped in x and y) tiles,
@@ -11,7 +13,7 @@ function [ijk0_from_landmark_index, foreground_p_value_from_landmark_index, imag
             [ (tile_shape_ijk(1)-1) - flipped_ijk0_from_landmark_index(:,1) ...
               (tile_shape_ijk(2)-1) - flipped_ijk0_from_landmark_index(:,2) ...
               flipped_ijk0_from_landmark_index(:,3) ] ;
+        foreground_p_value_from_landmark_index = landmark_data(:,4) ;
+        imagery_value_from_landmark_index = landmark_data(:,5) ;
     end
-    foreground_p_value_from_landmark_index = landmark_data(:,4) ;
-    imagery_value_from_landmark_index = landmark_data(:,5) ;
 end
